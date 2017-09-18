4040501c15e204353bbe6e9eee589a18221ad402
Yi Gao <yigao1983@gmail.com>
2017-09-18 16:42:35 -0700

    Smart pointer for spreadsheetcellmain

--- spreadsheetcellmain.cpp
+++ spreadsheetcellmain.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include "spreadsheetcell.hpp"
 
 using namespace std;
@@ -8,6 +9,10 @@ int main(int argc, char *argv[])
   SpreadsheetCell myCell, anotherCell;
   myCell.setValue(6);
   anotherCell.setString("3.2");
+  
+  unique_ptr<SpreadsheetCell> cellPtr(new SpreadsheetCell);
+  
+  cellPtr->setValue(3.7);
   
   return 0;
 }
